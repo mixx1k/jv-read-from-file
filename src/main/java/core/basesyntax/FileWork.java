c3a9ac32cb0d9464ae1874953bd24b5d0b75d870
michal <mihacik228@gmail.com>
2025-05-30 02:52:58 +0200

    Implement readFromFile method: read words starting with 'w' and return sorted array

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileWork {
+    private static final char TARGET_LETTER = 'w';
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        ArrayList<String> words = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] splited = line.split("[\\s,.!?;:\"()\\[\\]{}<>-]+");
+
+                for (String word : splited) {
+                    if (!word.isEmpty() && Character.toLowerCase(word.charAt(0)) == TARGET_LETTER) {
+                    words.add(word.toLowerCase());
+                    }
+                }
+            }
+        }  catch (IOException e) {
+            throw new RuntimeException("Can't read file", e);
+        }
+        words.sort(String::compareTo);
+        return words.toArray(new String[0]);
     }
 }
